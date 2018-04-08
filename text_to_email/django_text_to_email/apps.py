53f3e35b76e88a19a5956caf7d509a0096c9ec83
drd0013 <drd0013@gmail.com>
2018-04-08 14:21:11 -0700

    make django_text_to_email app and use urls

---
+++ text_to_email/django_text_to_email/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DjangoTextToEmailConfig(AppConfig):
+    name = 'django_text_to_email'
---
+++ text_to_email/django_text_to_email/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def first_view(request):
+    return HttpResponse(200)
